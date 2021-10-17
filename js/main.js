8cc989e2dd2ffc454e6dc3a67c5ac4afa7ab361f
Utkarsh_190 <utkarshtyagi62@gmail.com>
2021-10-17 22:49:34 +0530

    updated main.js for movie.html

--- js/main.js
+++ js/main.js
@@ -33,3 +33,54 @@ function getMovies(searchText) {
       console.log(err);
     });
 }
+
+function movieSelected(id) {
+  sessionStorage.setItem("movieID", id);
+  window.location = "movie.html";
+  return false;
+}
+
+function getMovie() {
+  let movieID = sessionStorage.getItem("movieID");
+
+  axios
+    .get("http://www.omdbapi.com/?apikey=12b235ec&i=" + movieID)
+    .then((response) => {
+      console.log(response);
+      let movie = response.data;
+
+      let output = `
+        <div class="row">
+          <div class="col-md-4">
+            <img src="${movie.Poster}" class="img-thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h2>${movie.Title}</h2>
+            <ul class="list-group">
+            <li class="list-group-item"><strong>Genre: </strong>${movie.Genre}</li>
+            <li class="list-group-item"><strong>Released: </strong>${movie.Released}</li>
+            <li class="list-group-item"><strong>Rated: </strong>${movie.Rated}</li>
+            <li class="list-group-item"><strong>IMDB Rating: </strong>${movie.imdbRating}</li>
+            <li class="list-group-item"><strong>Director: </strong>${movie.Director}</li>
+            <li class="list-group-item"><strong>Writer: </strong>${movie.Writer}</li>
+            <li class="list-group-item"><strong>Actors: </strong>${movie.Actors}</li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="card-body">
+            <h3 class="card-title">Plot</h3>
+            ${movie.Plot}
+            <hr/>
+
+            <a href="http://www.imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+            <a href="index.html" class="btn btn-secondary">Go back to search</a>
+          </div>
+        </div>
+      `;
+      $("#movie").html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
